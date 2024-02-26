0216880a4ab9fed82f1abb7b92ab926a4313bc7b
Joshua-Wacks <joshuawa@bbd.co.za>
2024-02-26 13:50:00 +0200

    Now reads file in memory

--- chatbot_mvp_demo.py
+++ chatbot_mvp_demo.py
@@ -7,9 +7,13 @@ import ssl
 import numpy as np
 from PIL import Image
 from io import BytesIO
-# import cv2
+import io
+
 
 from azure.storage.blob import ContainerClient
+
+from dotenv import load_dotenv
+load_dotenv()
 
 def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
@@ -106,6 +110,9 @@ def setup():
     
     if "global_btn_key" not in st.session_state:
         st.session_state.global_btn_key = 0
+        
+    if "query_running" not in st.session_state:
+        st.session_state.query_running = False
     
     # messages are the messages the user sees, we keep track of everything
     # chat_context is only the chat history we want to pass back
@@ -117,25 +124,38 @@ def setup():
     global new_chat_context
     new_chat_context = False
     if len(st.session_state.messages) == 0:
-        new_chat_context = True        
+        new_chat_context = True
+        
     else: 
         # Display chat messages from history on app rerun
         for message in st.session_state.messages:
             with st.chat_message(message["role"], avatar=getUserIcon(message["role"])):
                 content = message["content"]
-                if "sources" in message:                    
-                    st.markdown(content)
-                    add_sources(message["sources"])            
+                if "btn_type" in message:
+                    print(st.session_state.query_running)
+                    if st.session_state.query_running:
+                        print("IN")
+                        continue
+                    
+                    with st.expander("**Source:** " + message['source_file'].replace("target-dir\\","")):
+                        st.markdown(F"**Extract:** {content}")
+                        
+                        if message["btn_type"] == "general":
+                            create_button(message["btn_key"],message["source_file"])
+                        if message["btn_type"] == "faq":
+                            create_faq_button(message["btn_key"])
                 else:
-                    st.markdown(content)            
+                    st.markdown(content)
+                
+                
+            
     
 def create_button(btn_key,source_file):
-    st.session_state.global_btn_key = st.session_state.global_btn_key + 1 
     image_data = get_image_data(source_file)
     st.image(image_data,output_format="JPEG")
 
     st.download_button(
-        key = st.session_state.global_btn_key,
+        key = btn_key,
         label="Download Source",
         data=image_data,
         file_name="data_source.jpg",
@@ -143,12 +163,10 @@ def create_button(btn_key,source_file):
     )
 
 def create_faq_button(btn_key):
-    st.session_state.global_btn_key = st.session_state.global_btn_key + 1 
-
     faq_data = get_faq_data()
     
     st.download_button(
-        key = st.session_state.global_btn_key,
+        key = btn_key,
         label="Download FAQS",
         data=faq_data,
         file_name="all_faqs.txt",
@@ -168,10 +186,10 @@ def get_image_data(file_path):
     download_stream = st.session_state.container_client.download_blob(blob_name)
     blob_bytes = download_stream.readall()
 
-    with open(file_path_parts[2], "wb") as file:
-        file.write(blob_bytes)
+    # with open(file_path_parts[2], "wb") as file:
+    #     file.write(blob_bytes)
         
-    image = Image.open(file_path_parts[2])
+    image = Image.open(io.BytesIO(blob_bytes))
 
     buf = BytesIO()
     image.save(buf, format="JPEG")
@@ -180,59 +198,24 @@ def get_image_data(file_path):
     return byte_im
 
 def get_faq_data():
-    faq_file_path = './faq_blob_download.txt'
-    
-    if "faqs_read" not in st.session_state:
-        st.session_state.faqs_read = True
 
-        container_client = st.session_state.faq_container_client
-        blob_generator = container_client.list_blobs()
-        
-    
-        for blob in blob_generator:
-            with open(file=faq_file_path, mode="wb") as blob_file:
-                print(f"Started downloading {blob}")
-                download_stream = container_client.download_blob(blob)
-                print(f"File downloaded, streaming blob into {faq_file_path}")
-                blob_file.write(download_stream.readall())
+    # if "faqs_read" not in st.session_state:
+    st.session_state.faqs_read = True
 
-    with open(faq_file_path) as file:
-        all_faqs= file.readlines()
+    container_client = st.session_state.faq_container_client
+    blob_generator = container_client.list_blobs()
+    all_faqs = ""
 
-    s = "\n".join(all_faqs) 
+    for blob in blob_generator:
+        download_stream = container_client.download_blob(blob)
+        blob_bytes = download_stream.readall()
+        all_faqs = blob_bytes.decode("utf-8")
     
-    return s
+    return all_faqs
 
-def add_sources(sources):
-    for source in sources:
-        source_message = source['text'].encode('ascii', errors='ignore').decode().replace("\\n", "")
-    
-        with st.expander("**Source:** " + source['file'].replace("target-dir\\","")):
-            st.markdown(F"**Score:** {source['score']}")
-            st.markdown(F"**Extract:** {source_message}")
-            
-            btn_key = source['id'] + '0'
-            
-            if source['file'] != 'FAQ_file':
-                create_button(btn_key,source['file'])
-                #st.session_state.messages.append({"role": "assistant", "content": source_message, "btn_type": "general", "btn_key": source['id'], "source_file": source['file'], "score": source['score']})
-                
-            else:
-                create_faq_button(btn_key)
-                #st.session_state.messages.append({"role": "assistant", "content": source_message, "btn_type": "faq", "btn_key": source['id'], "source_file": source['file'], "score": source['score']})
-                        
 
 def react_to_message():
                 
-    # React to user input
-    # if prompt := st.chat_input("How can I assist?"):
-    #     with st.chat_message("assistant"):
-    #         st.markdown("Thanks")
-            
-    #         for i in range(4):
-    #             st.markdown(i)
-    #             create_button(i,"target-dir\\discovery-card-miles-terms-and-conditions_parts\\discovery-card-miles-terms-and-conditions_part_7.jpg")
-                # st.button(key = i,label = "View Source", on_click = show_user_source,kwargs = {"file_path": "target-dir\discovery-card-miles-terms-and-conditions_parts\discovery-card-miles-terms-and-conditions_part_7.jpg"})
     if prompt := st.chat_input("How can I assist?"):
         
         # Display user message in chat message container
@@ -242,46 +225,44 @@ def react_to_message():
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.session_state.chat_context.append({"role": "user", "content": prompt})
         
-
+        st.session_state.query_running = True
+        
         result = query_rag_pipeline(prompt,st.session_state.chat_context,new_context = new_chat_context)
+        st.session_state.query_running = False
+        
         # print(result)
         response = result['response']
         sources = result['sources']
-        # source_message = response + '\n'
+ 
+        
+            # source_message = response + '\n'
 
         
         with st.chat_message("assistant", avatar=getUserIcon("assistant")):
             st.markdown(response)
             st.session_state.chat_context.append({"role": "assistant", "content": response})
-            st.session_state.messages.append({"role": "assistant", "content": response, "sources" : sources})
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
-            add_sources(sources)
+
+            for source in sources:
+                source_message = source['text'].encode('ascii', errors='ignore').decode().replace("\\n", "")
             
-            
-                
-                # source_message = F"- Source: {source['file']} \n {source['score']} \n {source['text'].encode('ascii', errors='ignore')}\n\n\n"
-                # st.markdown(source_message)
-                
-                
-                # # source_message = source_message + F"- Source: {source['file']} \n {source['score']} \n {source['text'].encode('ascii', errors='ignore')}\n\n\n" 
-                # if source['file'] != 'FAQ_file':
-                #     create_button(source['id'],source['file'])
-                #     st.session_state.messages.append({"role": "assistant", "content": source_message, "btn_type": "general", "btn_key": source['id'], "source_file": source['file']})
-                # else:
-                #     create_faq_button(source['id'])
-                #     st.session_state.messages.append({"role": "assistant", "content": source_message, "btn_type": "faq", "btn_key": source['id']})
+                with st.expander("**Source:** " + source['file'].replace("target-dir\\","")):
+                    st.markdown(F"**Score:** {source['score']}")
+                    st.markdown(F"**Extract:** {source_message}")
                     
-                        # st.button(,label = "View Source", on_click = show_user_source,kwargs = {"file_path":})
-                
-            # st.markdown(source_message)
+                    st.session_state.global_btn_key = st.session_state.global_btn_key + 1 
+                    btn_key = st.session_state.global_btn_key
+                    
+                    if source['file'] != 'FAQ_file':
+                        create_button(btn_key,source['file'])
+                        st.session_state.messages.append({"role": "assistant", "content": source_message, "btn_type": "general", "btn_key": btn_key, "source_file": source['file']})
+                        
+                    else:
+                        create_faq_button(btn_key)
+                        st.session_state.messages.append({"role": "assistant", "content": source_message, "btn_type": "faq", "btn_key": btn_key, "source_file": source['file']})
 
-            
-        # complete_message = F"{response} + \n\n + {source_message}"
-        # # response = f"Echo: {prompt}"
-        # # Display assistant response in chat message container
-        # with st.chat_message("assistant"):
-        #     st.markdown(complete_message)
-        
+
         
         # Add assistant response to chat history
 
